5675da23a827d9c012e2cb40736f26da41f707c2
Daniel Santiago <danielsantiago@pop-os.localdomain>
2022-03-20 17:00:10 -0400

    Finished the simple HTML interface. Added functionality to JS file that has array of fun fact strings, and returns random string to be displayed on HTML page.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+//this randomFact function creates the array for the random facts, and returns a random fact to be displayed in the HTML file using the DOM
 let randomFact = () => {
     const factArray = ['The world is big', 'The moon is not real', 'I hate chickens'];
-    return Math.random(factArray);
+    let randomArray = factArray[Math.floor(Math.random() * factArray.length)];
+    return document.getElementById("message").innerHTML = randomArray;
+    
 }
