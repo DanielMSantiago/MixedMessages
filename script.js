a4b839fe15d514ae84240f38a95025d85738021a
Daniel Santiago <danielsantiago@pop-os.localdomain>
2022-03-20 17:02:42 -0400

    Organized array to make it easier to read

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 //this randomFact function creates the array for the random facts, and returns a random fact to be displayed in the HTML file using the DOM
 let randomFact = () => {
-    const factArray = ['The world is big', 'The moon is not real', 'I hate chickens'];
+    const factArray = [
+        'The world is big', 
+        'The moon is not real', 
+        'I hate chickens',
+        'The sky if blue'];
     let randomArray = factArray[Math.floor(Math.random() * factArray.length)];
-    return document.getElementById("message").innerHTML = randomArray;
+    return document.getElementById("message").innerHTML = "Fact: " + randomArray;
     
 }
